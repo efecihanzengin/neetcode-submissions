f8b7d5a99ed2b06e0643af2a55a4aaa957a0b210
Efe Cihan Zengin <efecihanzengin@gmail.com>
2026-04-28 09:21:36 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "efe"
+print(type(variable))
+variable = [1]
+print(type(variable))

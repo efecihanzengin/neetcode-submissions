2d6958124454678bbc73bd8642efb2827184f95c
Efe Cihan Zengin <efecihanzengin@gmail.com>
2026-04-28 09:44:33 +0300

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,11 @@
+variable = 10
+variable = str(variable)
+
+# Don't modify the code below this line
+print(variable)
+print(type(variable))
+
+variable = int(variable)
+
+print(variable)
+print(type(variable))
